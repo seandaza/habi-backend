a1846f3eb55b4c24d25ebdbff632eade1809dc03
sean <seandaza@gmail.com>
2022-03-30 21:14:27 -0500

    Server

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+class Server{
+    constructor(){ 
+        // Crear aplicacion express
+        this.app = express();
+        // Indicar el procesamiento de datos en formato json durante las peticiones
+        this.app.use(express.json());
+        // Almacenar el puerto en el que se ejecutara el servidor
+        this.app.set('PORT', process.env.PORT || 3000);
+        // crear ruta raiz
+        let router = express.Router();
+        // procesar solicitudes con el metodo GET a la raiz del servidor
+        router.get('/', (req, res) => {
+            res.status(200).send();
+        });
+
+        // Anadir ruta a express
+        this.app.use(router);
+        // Poner el servidor a escuchar en el puerto indicado
+        this.app.listen(this.app.get('PORT'), () => {
+            console.log('Server on port', this.app.get('PORT'));
+        });
+    }
+}
+
+new Server();
